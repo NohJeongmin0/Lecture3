63e5bad567586e4af0d8ea780a44681079e9ef77
NohJeongmin0 <njm7918@yonsei.ac.kr>
2025-09-18 17:24:24 +0900

    Add files via upload

---
+++ Lecture3 - 2.py
@@ -0,0 +1,26 @@
+class A:
+    def hello(self):
+        print("Hello from A")
+
+class B(A):
+    def hello(self):
+        print("Hello from B")
+        super().hello()
+
+class C(A):
+    def hello(self):
+        print("Hello from C")
+        super().hello()
+
+class D(B, C):
+    def hello(self):
+        print("Hello from D")
+        super().hello()
+
+class E(C):
+    pass
+
+class F(B, E):
+    pass
+
+print(F.mro())
